b8848fc70e9f403f98a3e5ef2bd6f8284583a876
Joscha <neske.joscha@gmail.com>
2023-03-06 21:39:07 +0100

    style: domain specified

--- src/helpers/documentation-code.ts
+++ src/helpers/documentation-code.ts
@@ -2,7 +2,7 @@ export const nodejs = `const axios = require("axios");
 
 const options = {
     method: 'POST',
-    url: 'https://textsimilarity.com/api/v1/similarity',
+    url: 'https://similarityapi.com/api/v1/similarity',
     params: {
       text1: 'First text',
       text2: 'Second text'
@@ -20,7 +20,7 @@ axios.request(options).then(function (response) {
 
 export const python = `import requests
 
-url = 'https://textsimilarity.com/api/v1/similarity'
+url = 'https://similarityapi.com/api/v1/similarity'
 api_key = 'YOUR_API_KEY'
 text1 = 'First text'
 text2 = 'Second text'
