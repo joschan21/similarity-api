19db83b64c6f2f00e620c166c89a2e2981f61da8
Joscha <neske.joscha@gmail.com>
2023-03-06 21:23:31 +0100

    fix: csp fix

--- next.config.js
+++ next.config.js
@@ -35,7 +35,7 @@ const nextConfig = {
           {
             key: 'Content-Security-Policy',
             value:
-              "default-src 'self'; frame-src: 'self'; script-src 'self' https: 'unsafe-inline'; connect-src 'self'; img-src 'self'; style-src 'self' https: 'unsafe-inline'; frame-ancestors 'self'; form-action 'self';",
+              "default-src 'self'; frame-src 'self'; script-src 'self' https: 'unsafe-inline'; connect-src 'self'; img-src 'self'; style-src 'self' https: 'unsafe-inline'; frame-ancestors 'self'; form-action 'self';",
           },
         ],
       },
