3b57867fcbb314f5d8f55ddb1a97f7af92b367fb
Stefano Gandolfo <49209915+sgandolfo@users.noreply.github.com>
2023-05-01 21:53:19 +0200

    Changed Code component make it work with new method of UseTheme

--- src/components/ui/Code.tsx
+++ src/components/ui/Code.tsx
@@ -1,80 +1,93 @@
-'use client'
+'use client';
 
-import { useTheme } from 'next-themes'
-import Highlight, { defaultProps, type Language } from 'prism-react-renderer'
-import darkTheme from 'prism-react-renderer/themes/nightOwl'
-import lightTheme from 'prism-react-renderer/themes/nightOwlLight'
-import { FC, useEffect, useState } from 'react'
+import { FC, useEffect, useState } from 'react';
+import { Highlight, themes } from 'prism-react-renderer';
+import { useTheme } from 'next-themes';
+// import darkTheme from 'prism-react-renderer/themes/nightOwl';
+// import lightTheme from 'prism-react-renderer/themes/nightOwlLight';
 
 interface CodeProps {
-  code: string
-  show: boolean
-  language: Language
-  animationDelay?: number
-  animated?: boolean
+  code: string;
+  show: boolean;
+  language: string;
+  animationDelay?: number;
+  animated?: boolean;
 }
 
 const Code: FC<CodeProps> = ({
   code,
+  language,
   show,
   animated,
   animationDelay,
-  language,
 }) => {
-  const { theme: applicationTheme } = useTheme()
-  const [text, setText] = useState<string>(animated ? '' : code)
+  const { theme: applicationTheme } = useTheme();
+  const [text, setText] = useState(animated ? '' : code);
 
   useEffect(() => {
     if (show && animated) {
-      let i = 0
+      let i = 0;
       setTimeout(() => {
         const intervalId = setInterval(() => {
-          setText(code.slice(0, i))
-          i++
+          setText(code.slice(0, i));
+          i++;
           if (i > code.length) {
-            clearInterval(intervalId)
+            clearInterval(intervalId);
           }
-        }, 15)
+        }, 15);
 
-        return () => clearInterval(intervalId)
-      }, animationDelay || 150)
+        return () => clearInterval(intervalId);
+      }, animationDelay || 150);
     }
-  }, [code, show, animated, animationDelay])
+  }, [code, show, animated, animationDelay]);
 
-  // number of lines
-  const lines = text.split(/\r\n|\r|\n/).length
+  //number of lines
+  const lines = text.split(/\r\n|\r|\n/).length;
 
-  const theme = applicationTheme === 'light' ? lightTheme : darkTheme
+  const theme =
+    applicationTheme === 'light' ? themes.nightOwlLight : themes.nightOwl;
 
   return (
-    <Highlight {...defaultProps} code={text} language={language} theme={theme}>
+    <Highlight
+      code={text}
+      language={language}
+      theme={theme}>
       {({ className, tokens, getLineProps, getTokenProps }) => (
         <pre
           className={
             className +
-            'transition-all w-fit bg-transparent duration-100 py-0 no-scrollbar'
+            'transition-all w-fit bg-transparten duration-100 py-0 no-scrollbar'
           }
           style={{
             maxHeight: show ? lines * 24 : 0,
             opacity: show ? 1 : 0,
           }}>
           {tokens.map((line, i) => {
-            // eslint-disable-next-line no-unused-vars
-            const { key, ...rest } = getLineProps({ line, key: i })
+            //esline-disable-next-line no-unused-vars
+            const { key, ...rest } = getLineProps({ line, key: i });
+
             return (
-              <div key={`line-${i}`} style={{ position: 'relative' }} {...rest}>
+              <div
+                key={`line-${i}`}
+                style={{ position: 'relative' }}
+                {...rest}>
                 {line.map((token, index) => {
-                  // eslint-disable-next-line no-unused-vars
-                  const { key, ...props } = getTokenProps({ token, i })
-                  return <span key={index} {...props} />
+                  //eslint-disable-next-line no-unused-vars
+                  const { key, ...props } = getTokenProps({ token, i });
+                  return (
+                    <span
+                      key={index}
+                      {...props}
+                    />
+                  );
                 })}
               </div>
-            )
+            );
           })}
         </pre>
       )}
     </Highlight>
-  )
-}
+  );
+};
 
-export default Code
+export default Code;
