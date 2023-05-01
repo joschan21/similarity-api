0d080acbb306e5abdb23f09e918cc00e8c6bdbc5
Stefano Gandolfo <49209915+sgandolfo@users.noreply.github.com>
2023-05-01 21:50:17 +0200

    Implement workaround for Redis to work

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,56 +1,58 @@
-import { getToken } from 'next-auth/jwt'
-import { withAuth } from 'next-auth/middleware'
-import { NextResponse } from 'next/server'
-import { Ratelimit } from '@upstash/ratelimit'
-import { Redis } from '@upstash/redis'
+import { getToken } from 'next-auth/jwt';
+import { withAuth } from 'next-auth/middleware';
+import { NextResponse } from 'next/server';
+import { Ratelimit } from '@upstash/ratelimit';
+import { Redis } from '@upstash/redis';
 
-const redis = new Redis({
-  url: process.env.REDIS_URL,
-  token: process.env.REDIS_SECRET,
-})
+const redis = Redis.fromEnv(); //Workaround to fix TypeScript error with normal way of creating Redis instance
+
+// const redis = new Redis({
+//   url: process.env.REDIS_URL,
+//   token: process.env.REDIS_SECRET,
+// });
 
 const ratelimit = new Ratelimit({
   redis: redis,
   limiter: Ratelimit.slidingWindow(50, '1 h'),
-})
+});
 
 export default withAuth(
   async function middleware(req) {
-    const pathname = req.nextUrl.pathname // relative path
+    const pathname = req.nextUrl.pathname; // relative path
 
     // Manage rate limiting
     if (pathname.startsWith('/api')) {
-      const ip = req.ip ?? '127.0.0.1'
+      const ip = req.ip ?? '127.0.0.1';
       try {
-        const { success } = await ratelimit.limit(ip)
+        const { success } = await ratelimit.limit(ip);
 
-        if (!success) return NextResponse.json({ error: 'Too Many Requests' })
-        return NextResponse.next()
+        if (!success) return NextResponse.json({ error: 'Too Many Requests' });
+        return NextResponse.next();
       } catch (error) {
-        return NextResponse.json({ error: 'Internal Server Error' })
+        return NextResponse.json({ error: 'Internal Server Error' });
       }
     }
 
     // Manage route protection
-    const token = await getToken({ req })
-    const isAuth = !!token
-    const isAuthPage = req.nextUrl.pathname.startsWith('/login')
+    const token = await getToken({ req });
+    const isAuth = !!token;
+    const isAuthPage = req.nextUrl.pathname.startsWith('/login');
 
-    const sensitiveRoutes = ['/dashboard']
+    const sensitiveRoutes = ['/dashboard'];
 
     if (isAuthPage) {
       if (isAuth) {
-        return NextResponse.redirect(new URL('/dashboard', req.url))
+        return NextResponse.redirect(new URL('/dashboard', req.url));
       }
 
-      return null
+      return null;
     }
 
     if (
       !isAuth &&
       sensitiveRoutes.some((route) => pathname.startsWith(route))
     ) {
-      return NextResponse.redirect(new URL('/login', req.url))
+      return NextResponse.redirect(new URL('/login', req.url));
     }
   },
   {
@@ -59,12 +61,12 @@ export default withAuth(
         // This is a work-around for handling redirect on auth pages.
         // We return true here so that the middleware function above
         // is always called.
-        return true
+        return true;
       },
     },
   }
-)
+);
 
 export const config = {
   matcher: ['/', '/login', '/dashboard/:path*', '/api/:path*'],
-}
+};
